17830c832bc119dd8f927d21b4a129140335fb37
Alan Cullinan <alancullinan@gmail.com>
2025-03-09 11:25:44 +0000

    Mod to player model

--- MatchTracker/Models/PlayerModel.swift
+++ MatchTracker/Models/PlayerModel.swift
@@ -13,10 +13,12 @@ class Player {
     @Attribute(.unique) var id: UUID
     var name: String = ""
     var jerseyNumber: Int = 0
+    var position: String = ""
 
-    init(name: String, jerseyNumber: Int) {
+    init(name: String, jerseyNumber: Int, position: String) {
         self.id = UUID()
         self.name = name
         self.jerseyNumber = jerseyNumber
+        self.position = position
     }
 }
